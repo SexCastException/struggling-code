b06f2759deb3f8a36f9e9a3d7a82bb804eaf969c
pyh <739967221@qq.com>
2019-09-24 17:06:32 +0800

    swagger分组、根据不同的项目环境是否开启swagger功能

--- swagger-learning-example/src/main/java/com/huazai/swagger/controller/HelloController.java
+++ swagger-learning-example/src/main/java/com/huazai/swagger/controller/HelloController.java
@@ -1,23 +1,29 @@
 package com.huazai.swagger.controller;
 
 import com.huazai.swagger.entity.User;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "测试接口",tags = {"测试接口"})
 @RestController
 public class HelloController {
+    @ApiOperation("hello请求")
     @GetMapping(value = "hello")
     public String hello(String s) {
         return s;
     }
 
-    @ApiOperation("返回用户信息请求")
-    @PostMapping("user")
-    public User getUser() {
-        return new User("huazai", "*******");
+    @ApiOperation("用户登录请求")
+    @PostMapping("login")
+    public User login(@ApiParam("dfaf") User user) {
+        return user;
     }
 
+    @ApiOperation("测试请求")
     @GetMapping("test1")
     public String test1(@ApiParam("用户名") String username) {
         return "huazai@qq.com";
--- swagger-learning-example/src/main/java/com/huazai/swagger/entity/User.java
+++ swagger-learning-example/src/main/java/com/huazai/swagger/entity/User.java
@@ -1,10 +1,14 @@
 package com.huazai.swagger.entity;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel
+@ApiModel("用户实体类")
 public class User {
+    @ApiModelProperty("用户名")
     private String username;
+    @ApiModelProperty("密码")
     private String password;
 
     public User(String username, String password) {
